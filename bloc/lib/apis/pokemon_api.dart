0765f03379784809b85cd5b70ec0971d3ea41189
Actinium825 <cruz.d.ace@proton.me>
2025-10-04 21:59:11 +0800

    feat: [Bloc] get pokemon list
    - add typedef for pokemon list
    - add getting pokemon list method in pokemon api and app cubit
    - add init pokemon list page method in app cubit and use in pokemon list page
    - add pokemon list property in app state
    - remove assigning local simple pokemon list variable in get more pokemon action in async redux

--- bloc/lib/apis/pokemon_api.dart
+++ bloc/lib/apis/pokemon_api.dart
@@ -1,5 +1,8 @@
 import 'package:pokedex_flutter_bloc/apis/api_client.dart';
+import 'package:pokedex_flutter_bloc/apis/model/pokemon.dart';
+import 'package:pokedex_flutter_bloc/apis/model/simple_pokemon.dart';
 import 'package:pokedex_flutter_bloc/apis/model/simple_pokemon_list.dart';
+import 'package:pokedex_flutter_bloc/utils/extension.dart';
 import 'package:pokedex_flutter_bloc/utils/typedef.dart';
 
 class PokemonApi {
@@ -13,5 +16,19 @@ class PokemonApi {
     final simplePokemonList = SimplePokemonList.fromJson(response.data!);
 
     return simplePokemonList;
+  }
+
+  Future<PokemonList> getPokemonList({required List<SimplePokemon> simplePokemonList}) async {
+    final dio = apiClient.dio;
+
+    final futures = simplePokemonList.forLoop((simplePokemon) {
+      final url = simplePokemon.detailsUrl;
+      final response = dio.get<Json>(url);
+      return response;
+    });
+
+    final responses = await Future.wait(futures);
+
+    return responses.forLoop((response) => Pokemon.fromJson(response.data!));
   }
 }
--- bloc/lib/utils/typedef.dart
+++ bloc/lib/utils/typedef.dart
@@ -1,1 +1,5 @@
+import 'package:pokedex_flutter_bloc/apis/model/pokemon.dart';
+
 typedef Json = Map<String, dynamic>;
+
+typedef PokemonList = List<Pokemon>;
