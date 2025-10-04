0cf2bdf11688794ac841fb5c7820ed16e5058572
Actinium825 <cruz.d.ace@proton.me>
2025-10-04 20:43:53 +0800

    feat: [Bloc] pokemon models
    - add models for official artwork, other sprites, pokemon, pokemon ability, pokemon base stat, pokemon info, pokemon move, pokemon sprites, and pokemon type

---
+++ bloc/lib/apis/model/pokemon_ability.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pokedex_flutter_bloc/apis/model/pokemon_info.dart';
+import 'package:pokedex_flutter_bloc/utils/typedef.dart';
+
+part 'pokemon_ability.freezed.dart';
+part 'pokemon_ability.g.dart';
+
+@freezed
+abstract class PokemonAbility with _$PokemonAbility {
+  const factory PokemonAbility({
+    @Default(PokemonInfo()) @JsonKey(name: 'ability') PokemonInfo abilityInfo,
+  }) = _PokemonAbility;
+
+  factory PokemonAbility.fromJson(Json json) => _$PokemonAbilityFromJson(json);
+}
