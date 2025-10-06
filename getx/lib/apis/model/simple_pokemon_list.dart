bcb68627d7d77149aa9ecd5ce451fca32abe972b
Actinium825 <cruz.d.ace@proton.me>
2025-10-06 08:30:49 +0800

    feat: [Get] simple pokemon models
    - add freezed, freezed annotation, build runner, json annotation, and json serializable packages
    - exclude generated files for analyzer in analysis options
    - ignore invalid annotation target errors in analysis options
    - git ignore generated files
    - create build yaml and set json serializable and freezed
    - add simple pokemon and list models
    - add typedef for json
    - revert generate condition step in review workflow

---
+++ getx/lib/apis/model/simple_pokemon_list.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:getx/apis/model/simple_pokemon.dart';
+import 'package:getx/utils/typedefs.dart';
+
+part 'simple_pokemon_list.freezed.dart';
+part 'simple_pokemon_list.g.dart';
+
+@freezed
+abstract class SimplePokemonList with _$SimplePokemonList {
+  const factory SimplePokemonList({
+    @Default(0) @JsonKey(name: 'count') int count,
+    @Default('') @JsonKey(name: 'next') String next,
+    @Default('') @JsonKey(name: 'previous') String previous,
+    @Default(<SimplePokemon>[]) @JsonKey(name: 'results') List<SimplePokemon> simplePokemonList,
+  }) = _SimplePokemonList;
+
+  factory SimplePokemonList.fromJson(Json json) => _$SimplePokemonListFromJson(json);
+}
